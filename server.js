815671e1378ab5039f73d41b83d6605a89a35493
LenerWeb <lener20@hotmail.com>
2025-10-17 22:27:33 -0500

    conexion exitosa del servidor

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { pool } from "./config/db.js";
+
+
+// import routes
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// rutas
+
+// prueba de conexion a la base de datos
+app.get("/api/test-db", async (req, res) =>{
+    try{
+        const[rows] = await pool.query("SELECT NOW() AS fecha");
+        res.json({ ok: true, conexion: "exitosa", fecha: rows[0].fecha });
+    } catch (err) {
+        res.status(500).json({ ok: false, error: err.message });
+    }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}` ));
